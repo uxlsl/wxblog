865a53cce19c14f6132ae294d9ccfeffcbb7f6ec
lsl <songlin.lin@yundata.com>
2018-07-22 08:51:42 +0800

    客户端修改

--- client/pages/index/index.js
+++ client/pages/index/index.js
@@ -1,21 +1,30 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+const url = "http://localhost:5000/list"
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    listblog: [{
+    list: [{
       tid:123, title: 'foo',
     }, {
       tid:345, title: 'bar'
     }]
   },
+  onLoad: function () {
+    var that = this
+    wx.request({
+      url: url,
+      success: function(res){
+        console.log(res.data)
+        that.setData({
+          list:res.data
+        })
+      }
+    }
+    )
+  },
   show_detail: function(e){
     console.log(e)
-    console.log(e.currentTarget.dataset.tid)
+    console.log(e.currentTarget.dataset.filename)
   }
 })
